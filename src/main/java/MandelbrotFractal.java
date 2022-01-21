9cb024984a7f38e15684d601bd4a1e7b955c7bd8
Дмитрий Кузнецов <dmitrijkuznecov@MacBook-Air-Dmitrij.local>
2022-01-21 21:58:50 +0300

    add black empty Jpanel

--- src/main/java/MandelbrotFractal.java
+++ src/main/java/MandelbrotFractal.java
@@ -1,10 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class MandelbrotFractal extends JFrame {
     private static final int WIDTH = 600;
     private static final int HEIGHT = 600;
     private static final String TITLE = "Mandelbrot fractal";
 
+    Canvas canvas;
+    BufferedImage bufferedImage;
 
     {
         setTitle(TITLE);
@@ -12,9 +16,31 @@ public class MandelbrotFractal extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
+        addCanvas();
         setVisible(true);
     }
+
+    private void addCanvas() {
+        canvas = new Canvas();
+        bufferedImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+        canvas.setBackground(Color.BLACK);
+        canvas.setVisible(true);
+        add(canvas, BorderLayout.CENTER);
+    }
+
     public static void main(String[]args){
         new MandelbrotFractal();
+    }
+
+    class Canvas extends JPanel{
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(WIDTH, HEIGHT);
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            g.drawImage(bufferedImage, 0, 0, null);
+        }
     }
 }
